81ce8b6a8bfa3e9f98e3027b5e118484843727d6
Ayumi ANDO <ayumi.ando@progmind.jp>
2016-11-09 11:55:08 +0900

    first commit

---
+++ MangaDB.py
@@ -0,0 +1,67 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import os
+import dropbox
+import qrcode
+
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+#dbx = dropbox.Dropbox("oFqZN8vwQaAAAAAAAAAADM46ud5QfR2dxjYBbaW31rU4uYFFPoCLkTV9omj7YnHh")
+dbx = dropbox.Dropbox("BTJ8jont9rAAAAAAAAAACUQN0vOOs9wbYX8ptkHTL_kxW34xGgN3aSwxws0cuiqB")
+QRsavedir = 'C:/Users/ayumi.ando/Desktop/test'
+
+def getext(filename):
+    return os.path.splitext(filename)[-1].lower()
+
+class ChangeHandler(FileSystemEventHandler):
+    print('★監視しているフォルダ')
+    print('　'+ os.path.abspath(os.path.dirname(__file__)))
+    print('★QRコードを保存するフォルダ')
+    print('　'+ QRsavedir)
+    
+    def on_created(self, event):
+        if event.is_directory:
+            return
+        if getext(event.src_path) in ('.jpg','.png','.txt'):	
+            print('\n◆マンガが生成されました')
+            #print('　ファイルパスは %s です\n' % event.src_path)
+            path = event.src_path
+            print('　Dropboxにアップロードします')
+            file = os.path.basename(path)
+            print('　ファイル名は' + file + 'です\n')
+
+            with open(file, 'rb') as f:
+                dbx.files_upload(f.read(), '/'+ file)
+            data = dbx.sharing_create_shared_link_with_settings('/' + file)
+            print('　アップロード完了しました。ファイルのURLは')
+
+            print('　' + data.url)
+            print('　です\n')
+            print('　QRコードの画像を生成します')
+
+            encode_text = data.url
+            img = qrcode.make(encode_text)
+            type(img)
+            img.save(QRsavedir +'/QR_'+ file)
+            print('　QRコードを表示＆保存しました\n')
+            img.show()
+
+
+
+
+if __name__ in '__main__':
+    while 1:
+        event_handler = ChangeHandler()
+        observer = Observer()
+        observer.schedule(event_handler,BASEDIR,recursive=True)
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
